d96022a66a849ba6078d5e7706af84216cf99252
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-10-25 15:57:47 -0200

    test crash unit base64

--- walletsdk/src/test/java/com/mundipagg/encoder/Base64Test.java
+++ walletsdk/src/test/java/com/mundipagg/encoder/Base64Test.java
@@ -12,9 +12,19 @@ import static org.junit.Assert.*;
 public class Base64Test {
 
     @Test
-    public void encode() throws Exception {
+    public void encode_success() throws Exception {
 
         String textToEncode = "sk_test_A0bkwbvFE2uOlWYE:";
+        String textEncoded = new Base64().encode(textToEncode);
+        String outputEncoded = "c2tfdGVzdF9BMGJrd2J2RkUydU9sV1lFOg==";
+
+        Assert.assertEquals(outputEncoded, textEncoded);
+    }
+
+    @Test
+    public void encode_error() throws Exception {
+
+        String textToEncode = "sk_test_A0bkwbvFE2uOlWYE"; // removing `:` for authorization
         String textEncoded = new Base64().encode(textToEncode);
         String outputEncoded = "c2tfdGVzdF9BMGJrd2J2RkUydU9sV1lFOg==";
 
