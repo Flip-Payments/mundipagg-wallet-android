4d0c69be3d25dad895a48ca3fb72eefc7c130a0d
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-10-28 06:09:31 -0200

    starting app

---
+++ app/src/main/java/com/mundipagg/walletdemo/activities/DemoBaseActivity.java
@@ -0,0 +1,10 @@
+package com.mundipagg.walletdemo.activities;
+
+import com.jgabrielfreitas.layoutid.activity.InjectLayoutBaseActivity;
+
+/**
+ * Created by JGabrielFreitas on 28/10/16.
+ */
+
+public class DemoBaseActivity extends InjectLayoutBaseActivity {
+}
