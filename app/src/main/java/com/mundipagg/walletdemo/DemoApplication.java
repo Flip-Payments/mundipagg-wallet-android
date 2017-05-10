4bad04ccaeee206033fb40acc8cca42fe9ad38a7
jcaiqueoliveira <j.caique_fla@hotmail.com>
2017-05-10 18:14:15 -0300

    update sample

--- app/src/main/java/com/mundipagg/walletdemo/DemoApplication.java
+++ app/src/main/java/com/mundipagg/walletdemo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.mundipagg.walletdemo;
 
 import com.jgabrielfreitas.permissions.core.application.CorePermissionsApplication;
 import com.mundipagg.MundipaggAccount;
+import com.mundipagg.MundipaggWallet;
 
 /**
  * Created by JGabrielFreitas on 26/09/16.
@@ -13,7 +14,7 @@ public class DemoApplication extends CorePermissionsApplication {
     public void onCreate() {
         super.onCreate();
 
-        // MundipaggWallet.init("VjnlLyaMJmGpRKAzL02ypojdwVMalRX3Bn8P1xdbyM3w5J0Gj2mo7rZEXqPgywrU5h38ev8aBALnVWK4RpOkNlz6Q91G69Qxr5bm7OZKAE4kJWNqv603x192wNOZqozW");
-        MundipaggAccount.getInstance().setCustomerId("cus_MQN3g7BsQUOO3Abd");
+        MundipaggWallet.init("token");
+        MundipaggAccount.getInstance().setCustomerId("customer_id");
     }
 }
